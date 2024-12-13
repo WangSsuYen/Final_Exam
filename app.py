df4a9b0d32bcba0172ea607b8a1c27c9844dde1c
WangSsuYen <a0909321155@icloud.com>
2024-12-14 01:29:55 +0800

    add app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from lib import *
+import re
 
 while True:
     search_key = input("請輸入搜尋詞 (輸入 'off' 結束): ")
@@ -10,8 +11,13 @@ while True:
         # 初始化爬蟲類別並爬取資料
         crawler = DataCrawl(search_key)
         result = crawler.crawl()
-
-        print(result)
+        # 處理 blocks
+        for key, value in result.items() :
+            for block_key, block_value in value.items():
+                if re.findall(r'^block', block_key):
+                    print(block_value)
+                else :
+                    print("This is phrase")
 
     except Exception as e:
         print(f"\n發生錯誤: {e}")
